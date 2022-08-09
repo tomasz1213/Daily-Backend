56492bcf434910dfc52da960487a815a1ff3cb4b
Tomasz Mlyczynski <t.mlyczynski@qarbon.it>
2022-08-09 08:59:48 +0200

    add icon api

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const HttpError = require('./models/http-error');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use('/upload/weather', express.static(path.join('upload', 'weather')));
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -17,12 +21,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-
-
 
 app.use('/api/user', userRoutes);
+
 app.use((req, res, next) => next(new HttpError('Could not find route', 404)));
 
 app.use((error, req, res, next) => {
--- middleware/auth-check.js
+++ middleware/auth-check.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => { 
     try {
         const token = req.headers.authorization.split(' ')[1];
-        console.log(req.headers);
         if (!token) {
             throw new Error('Authentication failed');
         }
